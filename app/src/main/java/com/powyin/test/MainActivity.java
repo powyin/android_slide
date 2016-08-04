2922b4e13138387ae61c77c75acd9c67a3facab0
yinhaoxiang <yinhaoxiang@gmail.com>
2016-08-04 15:56:05 +0800

    update UI

--- app/src/main/java/com/powyin/test/MainActivity.java
+++ app/src/main/java/com/powyin/test/MainActivity.java
@@ -1,39 +1,31 @@
 package com.powyin.test;
 
+import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.powyin.slide.widget.SlideSwitch;
 
 public class MainActivity extends AppCompatActivity {
-
-    SlideSwitch slideSwitch;
-    ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        slideSwitch = (SlideSwitch)findViewById(R.id.test_switch);
-        viewPager = (ViewPager)findViewById(R.id.view_pager);
-        viewPager.setAdapter(new PageAdapter(getSupportFragmentManager()));
+    }
 
-        viewPager.addOnPageChangeListener(slideSwitch.getSupportOnPageChangeListener());
-        viewPager.setOffscreenPageLimit(1);
-
-        slideSwitch.setOnPageChangeListener(new SlideSwitch.OnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                viewPager.setCurrentItem(position);
-            }
-
-            @Override
-            public void onPageScroll(float mScroll) {
-                System.out.println("-------zzz"+mScroll);
-            }
-        });
-
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.click_to_single:
+                startActivity(new Intent(this,SimpleSlideSwitch.class));
+                break;
+            case R.id.click_to_multiple:
+                startActivity(new Intent(this,SimpleSlideSwitchMultiple.class));
+                break;
+        }
 
 
     }
+
 }
---
+++ app/src/main/java/com/powyin/test/SimpleSlideSwitch.java
@@ -0,0 +1,48 @@
+package com.powyin.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.powyin.slide.widget.SlideSwitch;
+
+import java.lang.reflect.AccessibleObject;
+
+/**
+ * Created by powyin on 2016/8/4.
+ */
+public class SimpleSlideSwitch extends AppCompatActivity {
+
+    SlideSwitch slideSwitch;
+    ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.avtivity_simple_slide_switch);
+        slideSwitch = (SlideSwitch)findViewById(R.id.test_switch);
+        viewPager = (ViewPager)findViewById(R.id.view_pager);
+        viewPager.setAdapter(new PageAdapter(getSupportFragmentManager()));
+
+        viewPager.addOnPageChangeListener(slideSwitch.getSupportOnPageChangeListener());
+        viewPager.setOffscreenPageLimit(1);
+
+        slideSwitch.setOnPageChangeListener(new SlideSwitch.OnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                viewPager.setCurrentItem(position);
+            }
+
+            @Override
+            public void onPageScroll(float mScroll) {
+                System.out.println("-------zzz"+mScroll);
+            }
+        });
+
+
+
+    }
+
+
+
+}
